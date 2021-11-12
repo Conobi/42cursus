6a102ae8b602fee97545f0c909a79b32ccb45cb7
Conobi <Conobi@users.noreply.github.com>
2021-11-12 19:38:19 +0100

    Reverted ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,22 @@
 /*   By: conobi                                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 16:49:48 by conobi            #+#    #+#             */
-/*   Updated: 2021/11/12 15:53:42 by conobi           ###   ########lyon.fr   */
+/*   Updated: 2021/11/12 19:32:16 by conobi           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	f_inset(char const *set, char a)
+{
+	int	i;
+
+	i = -1;
+	while (set[++i])
+		if (a == set[i])
+			return (1);
+	return (0);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -22,9 +33,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	len = ft_strlen(s1) - 1;
 	start = 0;
-	while (ft_strchr(set, s1[start]))
+	while (f_inset(set, s1[start]))
 		start++;
-	while (ft_strchr(set, s1[len]) && len > start)
+	while (f_inset(set, s1[len]) && len > start)
 		len--;
 	ret = ft_substr(s1, start, len - start + 1);
 	return (ret);
