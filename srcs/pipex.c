988382aa91040421fa34ac6e692a6a13836048b7
Conobi <Conobi@users.noreply.github.com>
2022-01-21 18:53:23 +0100

    Almost working, still an issue on the path parsing and leaks

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: conobi                                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 14:50:09 by conobi            #+#    #+#             */
-/*   Updated: 2022/01/17 19:56:31 by conobi           ###   ########lyon.fr   */
+/*   Updated: 2022/01/21 18:05:53 by conobi           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,22 +54,97 @@ void	close_files(char *infile, char *outfile, t_data *data)
 	}
 }
 
-int	main(int argc, char *argv[], char **env)
+void	child_manager(t_data *data, char **cmd, short type)
+{
+	if (!type)
+	{
+		perr_cb("dup2", dup2(data->in_fd, STDIN_FILENO), -1);
+		perr_cb("dup2", dup2(data->piped[1], STDOUT_FILENO), -1);
+	}
+	else if (type == 1)
+	{
+		perr_cb("dup2", dup2(data->piped[0], STDIN_FILENO), -1);
+		perr_cb("dup2", dup2(data->out_fd, STDOUT_FILENO), -1);
+	}
+	else if (type == 2)
+	{
+		perr_cb("dup2", dup2(data->in_fd, STDIN_FILENO), -1);
+		perr_cb("dup2", dup2(data->out_fd, STDOUT_FILENO), -1);
+	}
+	// int	i;
+	// i = -1;
+	// while (data->env[++i])
+	// {
+	// 	ft_putstr_fd(data->env[i], 2);
+	// 	ft_putstr_fd("\n", 2);
+	// }
+	perr_cb("close", close(data->piped[0]), -1);
+	perr_cb("close", close(data->piped[1]), -1);
+	if (cmd[0])
+	{
+		perr_cb(cmd[0],
+			execve(cmd[0], cmd, data->env), -1);
+	}
+}
+
+void	single_manager(t_data *data, char **argv)
+{
+	pid_t	pid;
+	char	**cmd;
+
+	if (data->cmd1_args[0])
+		cmd = data->cmd1_args;
+	else
+		cmd = data->cmd2_args;
+	// printf("Single manager (%s)\n", cmd[0]);
+	pid = fork();
+	if (pid == -1)
+		perr_print("fork");
+	if (pid == 0)
+		child_manager(data, cmd, 2);
+	else
+		close_files(argv[1], argv[4], data);
+}
+
+void	double_manager(t_data *data, char **argv)
+{
+	pid_t	pid;
+
+	// printf("Double manager\n");
+	pid = fork();
+	if (pid == -1)
+		perr_print("fork");
+	if (pid == 0)
+		child_manager(data, data->cmd1_args, 0);
+	else
+	{
+		pid = fork();
+		if (pid == -1)
+			perr_print("fork");
+		if (pid == 0)
+			child_manager(data, data->cmd2_args, 1);
+	}
+	if (pid != 0)
+		close_files(argv[1], argv[4], data);
+}
+
+int	main(int argc, char **argv, char **env)
 {
 	t_data	data;
-	// pid_t	pid;
-	// int		status;
 
 	if (argc == 5)
 	{
+		data.env = env;
+		data.status = pipe(data.piped);
+		if (data.status != 0)
+			return (perr_print("pipe"));
 		open_files(argv[1], argv[4], &data);
-		data.cmd1_path = find_binary_path(argv[2], env);
-		if (!data.cmd1_path)
-			perr_print(argv[2]);
-		data.cmd2_path = find_binary_path(argv[3], env);
-		if (!data.cmd1_path)
-			perr_print(argv[3]);
-		close_files(argv[1], argv[4], &data);
+		slice_binary_args(argv[2], argv[3], &data);
+		double_manager(&data, argv);
+		// else if (!data.cmd1_args[0] && data.cmd2_args[0])
+		// 	single_manager(&data, argv);
+		// else
+		// 	close_files(argv[1], argv[4], &data);
 		return (0);
 	}
 	else
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: conobi                                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 17:42:21 by conobi            #+#    #+#             */
-/*   Updated: 2022/01/17 19:37:22 by conobi           ###   ########lyon.fr   */
+/*   Updated: 2022/01/21 18:52:36 by conobi           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,50 @@ int	perr_print(char *str)
 	return (1);
 }
 
-char	*find_binary_path(char *binary, char **env)
+int	perr_cb(char *errstr, int ret, int ver)
+{
+	if (ret == ver)
+		perr_print(errstr);
+	return (ret);
+}
+
+void	slice_binary_args(char *cmd1, char *cmd2, t_data *data)
+{
+	char	**cmd1_split;
+	char	**cmd2_split;
+
+	cmd1_split = ft_split(cmd1, ' ');
+	cmd2_split = ft_split(cmd2, ' ');
+	if (!cmd1_split || !cmd2_split)
+		return ((void)(perr_print("split")));
+	data->cmd1_args = cmd1_split;
+	data->cmd2_args = cmd2_split;
+	data->cmd1_args[0] = find_binary_path(data->cmd1_args[0], data);
+	if (!data->cmd1_args[0])
+		perr_print(cmd1);
+	data->cmd2_args[0] = find_binary_path(data->cmd2_args[0], data);
+	if (!data->cmd2_args[0])
+		perr_print(cmd2);
+}
+
+char	*find_binary_path(char *binary, t_data *data)
 {
 	int		i;
 	char	**path;
 	char	*bin_path;
 
 	i = 0;
-	while (env[i] && ft_strncmp(env[i], "PATH=", 5) != 0)
+	while (data->env[i] && ft_strncmp(data->env[i], "PATH=", 5) != 0)
 		i++;
-	if (ft_strncmp(env[i], "PATH=", 5) != 0)
+	if (ft_strncmp(data->env[i], "PATH=", 5) != 0)
 		return (0);
-	path = ft_split(env[i], ':');
+	path = ft_split(data->env[i], ':');
+	i = -1;
 	if (!path)
 		return (0);
 	i = 0;
 	bin_path = 0;
+
 	while (path[++i] && access(bin_path, F_OK) != 0)
 	{
 		free(bin_path);
