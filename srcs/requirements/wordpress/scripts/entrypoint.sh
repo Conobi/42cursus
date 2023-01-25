5e034772693101649ceff46e138953b821998e1d
Conobi <Conobi@users.noreply.github.com>
2023-01-25 02:30:47 +0100

    Added SSL to Nginx

--- srcs/requirements/wordpress/scripts/entrypoint.sh
+++ srcs/requirements/wordpress/scripts/entrypoint.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
 
-if ! wp core is-installed
+if ! wp core is-installed --allow-root
 then
-	wp core install \
+	wp core install --allow-root \
 		--url="$WP_DOMAIN" \
 		--title="$WP_TITLE" \
 		--admin_name="$WP_ADMIN" \
 		--admin_email="$WP_ADMIN_EMAIL" \
 		--admin_password="$WP_ADMIN_PASSWORD"
 fi
-
-echo $DB_HOST
-echo $DB_NAME
-echo $DB_USER
-echo $DB_PASSWORD
-echo $WP_DOMAIN
-echo $WP_ADMIN
-echo $WP_ADMIN_EMAIL
-echo $WP_ADMIN_PASSWORD
-echo $WP_TITLE
 
 php-fpm7.3 --nodaemonize
